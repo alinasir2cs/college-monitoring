f124bff412b5c9cb1cf3d23b6b94dd3c09ff6884
alinasir2cs <58271265+alinasir2cs@users.noreply.github.com>
2025-11-05 02:33:09 +0500

    Update KPI for unvisited college actions category

--- monitoring_action_report_dashboard_streamlit.py
+++ monitoring_action_report_dashboard_streamlit.py
@@ -192,7 +192,7 @@ actions_against_employees = df[df['Category'].astype(str).str.contains('Employee
 proxy_attendance = df[df['Reason'].astype(str).str.contains('Proxy Attendance', case=False, na=False)]
 
 # --- Define new KPI metrics ---
-unvisited_college_actions = df[df["Category"].str.contains("Unvisited", case=False, na=False)]
+unvisited_college_actions = df[df["Category"].str.contains("College Not visited", case=False, na=False)]
 habitual_absenteeism_actions = df[df["Reason"].str.contains("Habitual Absentiesm", case=False, na=False)]
 
 # --- Prepare KPI data ---
